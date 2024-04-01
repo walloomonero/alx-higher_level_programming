a6b4c32ea0b489f5776448d25182c6ce92a2a54b
walloomonero <wallaskhoza@gmail.com>
2024-04-01 12:17:57 +0200

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    try:
         with urllib.request.urlopen(url) as respon:
             body = respon.read().decode('utf-8')
             print(body)
