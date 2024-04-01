5df52a19bbdf20387b530c4618ec8fb97899a713
walloomonero <wallaskhoza@gmail.com>
2024-04-01 10:41:41 +0200

    python network first commit

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
